b4398924d51cd48a17d2272e6f5097865e82d761
ian <yihengz@outlook.com>
2023-06-20 20:46:58 +0800

    sub routes issue

--- src/routes/users/users.controller.ts
+++ src/routes/users/users.controller.ts
@@ -1,11 +1,57 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Head,
+  Options,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Head('rsvp')
+  hRsvp() {
+    return 'Head rsvp!';
+  }
+
+  @Options('rsvp')
+  oRsvp() {
+    return 'Options rsvp';
+  }
+
+  @Get('rsvp')
+  gRsvp() {
+    return 'Get rsvp';
+  }
+
+  @Post('rsvp')
+  pRsvp() {
+    return 'Post rsvp';
+  }
+
+  @Put('rsvp')
+  puRsvp() {
+    return 'Put rsvp';
+  }
+
+  @Patch('rsvp')
+  ptRsvp() {
+    return 'Patch rsvp';
+  }
+
+  @Delete('rsvp')
+  dRsvp() {
+    return 'Delete rsvp';
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
